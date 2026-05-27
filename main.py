44d5b21d267101a420c20259c96a006804e08bed
Rafał Palka <raflikpalka@gmail.com>
2026-05-27 09:47:22 +0200

    Added transport availability change

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 from customer import Customer
 from services import BikeTransportServices
 
+bts = BikeTransportServices()
+
 c1 = Customer("Albert")
-c1.order_transport(BikeTransportServices())
+c1.order_transport(bts)
+
+c2 = Customer("David")
+c2.order_transport(bts)
