697deca2af572776a24f3560a9366e88fb1fea55
M2017P <michal.pawlenka@gsuite.us.edu.pl>
2026-06-01 20:50:29 +0200

    Apply suggestion from @M2017P

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from customer import Customer
 from services import BikeTransportServices
 from services import Car_TransportServices
-from services import scooterTransportServices
+from services import ScooterTransportServices
 
 c1 = Customer("Albert")
 c1.order_transport(BikeTransportServices())
